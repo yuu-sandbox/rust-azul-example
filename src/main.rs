f61013b949539d663cc9b96fc3158402210665b9
yuu <yuuxzy@gmail.com>
2019-10-22 10:12:09 +0900

    Impl counter example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use azul::{prelude::*, widgets::{label::Label, button::Button}};
+
+struct Root {
+    counter: usize,
+}
+
+fn update_counter(event: CallbackInfo<Root>) -> UpdateScreen {
+    event.state.data.counter += 1;
+    Redraw
+}
+
+impl Layout for Root {
+    fn layout(&self, _: LayoutInfo<Self>) -> Dom<Self> {
+        let label = Label::new(format!("{}", self.counter)).dom();
+        let button = Button::with_label("update counter").dom()
+            .with_callback(On::MouseUp, update_counter);
+        Dom::div()
+            .with_child(label)
+            .with_child(button)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new(Root{counter: 0}, AppConfig::default()).unwrap();
+    let window = app.create_window(WindowCreateOptions::default(), css::native()).unwrap();
+    app.run(window).unwrap();
 }
